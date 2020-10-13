816af1e683e3eabf7f226e639f0b06e3ca528cfa
GeetikaBhagat <68065986+GeetikaBhagat@users.noreply.github.com>
2020-10-13 07:25:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var speed, weight;
 
 function setup() {
   createCanvas(1600, 400);
-  car = createSprite(50, 200, 50, 50);
+  car = createSprite(50, 200, 80, 50);
   car.shapeColor = 'blue';
 
   wall = createSprite(1500, 200, 60, height / 2);
@@ -22,15 +22,15 @@ function draw() {
   if (wall.x - car.x < car.width / 2 + wall.width / 2) {
     car.velocityX = 0;
 
-    var deformatiom = 0.5 * weight * speed * speed / 22500;
-
+    var deformation = 0.5*weight*speed*speed/22500;
+    console.log(deformation);
     if (deformation < 100) {
       car.shapeColor = 'green';
     }
-    else if (deformation > 100 && deformation < 180) {
+    if (deformation > 100 && deformation < 180) {
       car.shapeColor = 'yellow';
     }
-    else if (deformation > 180) {
+    if (deformation > 180) {
       car.shapeColor = 'red';
     }
   }
